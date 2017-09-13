3bc181ff83a16f3ed670e4e9c10f64e80b27abe7
Julien Marusi <julien.marusi@watchever.com>
2017-09-13 10:16:09 +0200

    Correctif scan.php

--- scan.php
+++ scan.php
@@ -60,7 +60,7 @@ function scan($dir){
 header('Content-type: application/json');
 
 echo json_encode(array(
-	"name" => "files",
+	"name" => $dir,
 	"type" => "folder",
 	"path" => $dir,
 	"items" => $response
